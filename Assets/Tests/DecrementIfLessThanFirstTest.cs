41f94a0f772c55b0f75db3a7b2c6ac68f992c35f
unknown <C00218004@itcarlow.ie>
2019-09-20 09:33:49 +0100

    completed filter and working test

---
+++ Assets/Tests/DecrementIfLessThanFirstTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class DecrementIfLessThanFirstTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void DecrementIfLessThanFirstTestBiggerThanFirst()
+        {
+            float[] inputArr = { 5.0f, 2.0f, 5.0f, 9.0f };
+            float[] expectedArr = { 5.0f, 1.0f, 5.0f, 9.0f };            
+
+            CollectionAssert.AreEqual(expectedArr, DecrementIfLessThanFirstFilter.DecrementIfLess(inputArr));
+        }
+    }
+}
