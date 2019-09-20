f792c5ee8d6dac3bcda955ea1be5a88797ec7083
unknown <C00218004@itcarlow.ie>
2019-09-20 12:05:04 +0100

    added multiple tests

--- Assets/Tests/DecrementIfLessThanFirstTest.cs
+++ Assets/Tests/DecrementIfLessThanFirstTest.cs
@@ -5,14 +5,38 @@ namespace Tests
 {
     public class DecrementIfLessThanFirstTest
     {
-        // A Test behaves as an ordinary method
         [Test]
-        public void DecrementIfLessThanFirstTestBiggerThanFirst()
+        public void DecrementSecondNumPass()
         {
             float[] inputArr = { 5.0f, 2.0f, 5.0f, 9.0f };
-            float[] expectedArr = { 5.0f, 1.0f, 5.0f, 9.0f };            
+            float[] expectedArr = { 5.0f, 1.0f, 5.0f, 9.0f };
 
             CollectionAssert.AreEqual(expectedArr, DecrementIfLessThanFirstFilter.DecrementIfLess(inputArr));
         }
+        [Test]
+        public void DecrementThirdNumPass()
+        {
+            float[] inputArr = { 5.0f, 6.0f, 4.0f, 9.0f };
+            float[] expectedArr = { 5.0f, 6.0f, 3.0f, 9.0f };
+
+            CollectionAssert.AreEqual(expectedArr, DecrementIfLessThanFirstFilter.DecrementIfLess(inputArr));
+        }
+        [Test]
+        public void DecrementFourthNumPass()
+        {
+            float[] inputArr = { 5.0f, 6.0f, 7.0f, 3.0f };
+            float[] expectedArr = { 5.0f, 6.0f, 7.0f, 2.0f };
+
+            CollectionAssert.AreEqual(expectedArr, DecrementIfLessThanFirstFilter.DecrementIfLess(inputArr));
+        }
+        [Test]
+        public void DecrementNone()
+        {
+            float[] inputArr = { 1.0f, 2.0f, 3.0f, 4.0f };
+            float[] expectedArr = { 1.0f, 2.0f, 3.0f, 4.0f };
+
+            CollectionAssert.AreEqual(expectedArr, DecrementIfLessThanFirstFilter.DecrementIfLess(inputArr));
+        }
+
     }
 }
