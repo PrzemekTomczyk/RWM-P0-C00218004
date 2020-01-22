86703bcfa06178fa790d24b3e7e6fb4f4cbd6d86
unknown <C00218004@itcarlow.ie>
2020-01-22 09:56:23 +0000

    fixed balloon still being anchored to a rigidBody if anchor was changed to a static position

---
+++ Assets/Scripts/ToBinaryFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ToBinaryFilter
+{
+    public static List<int> ToBinary(List<int> t_input)
+    {
+        List<int> result = new List<int>();
+        if (t_input != null && t_input.Count > 0)
+        {
+            foreach (int integer in t_input)
+            {
+                result.Add(Convert.ToInt32(Convert.ToString(integer, 2)));
+            }
+        }
+        return result;
+    }
+}
---
+++ Assets/Tests/ToBinaryFilterTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class ToBinary
+    {
+        [Test]
+        public void ToBinarySimplePasses()
+        {
+            List<int> input = new List<int> { 2, 4, 10 };
+            List<int> output = ToBinaryFilter.ToBinary(input);
+            List<int> expected = new List<int> { 10, 100, 1010};
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+        [Test]
+        public void ToBinaryBoundaryCases()
+        {
+            List<int> input = new List<int> { 0, 1, 1023 };
+            List<int> output = ToBinaryFilter.ToBinary(input);
+            List<int> expected = new List<int> { 0, 1, 1111111111 };
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+        [Test]
+        public void ToBinaryFail()
+        {
+            List<int> input = new List<int> { 0, 10, 100 };
+            List<int> output = ToBinaryFilter.ToBinary(input);
+            List<int> wrongValues = new List<int> { 2, 4, 6 };
+
+            CollectionAssert.AreNotEqual(output, wrongValues);
+        }
+    }
+}
